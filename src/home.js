22774cc20368366c1ea9706e19dca01c2f171773
Connor <cstreng95@gmail.com>
2020-04-23 10:04:39 +0200

    daily contributions

--- src/home.js
+++ src/home.js
@@ -18,8 +18,6 @@ const home = () => {
 
     const topDiv = document.createElement('div');
     topDiv.id = "topDiv";
-    //topDiv.style.gridRowStart = "1";
-    //topDiv.style.gridRowEnd = "2";
     topDiv.style.display = "flex";
     topDiv.style.justifyContent = "space-evenly";
     topDiv.style.alignItems = "center";
@@ -32,94 +30,120 @@ const home = () => {
     link1.style.display = "flex";
     link1.style.flexBasis = "10%";
     link1.style.height = "50px";
-    link1.style.fontSize = "20px";
-    link1.style.fontFamily = "Arial";
 
         const linkInner1 = document.createElement('div');
-        linkInner1.innerHTML = "LINK 1";
-        linkInner1.style.margin = "auto";
-        linkInner1.style.color = "white";
-        linkInner1.style.fontSize = "20px";
-        linkInner1.style.fontFamily = "Arial";
-
+        linkInner1.innerHTML = "MENU";
+        linkInner1.className = "link";
         link1.appendChild(linkInner1);
+
+    const nameDiv = document.createElement('div');
+    nameDiv.id = "nameDiv";
+    nameDiv.className = "title"
+    nameDiv.style.height = "110px";
+    nameDiv.style.width = "270px";
+    nameDiv.style.display = "flex";
+    nameDiv.style.border = "10px solid white";
+
+    const nameText = document.createElement('div');
+    nameText.style.margin = "auto";
+    nameText.className = "title";
+    nameText.innerHTML = "Dodo & Coco";
+    nameText.style.fontSize = "40px";
+    nameText.style.color = "white";
+    nameText.style.fontWeight = "bold";
+    nameText.style.fontFamily = "Arial";
 
     const link2 = document.createElement('div');
     link2.id = "link2";
     link2.style.display = "flex";
     link2.style.flexBasis = "10%";
     link2.style.height = "50px";
-    link2.style.fontSize = "20px";
-    link2.style.fontFamily = "Arial";
 
         const linkInner2 = document.createElement('div');
-        linkInner2.innerHTML = "LINK 2";
-        linkInner2.style.margin = "auto";
-        linkInner2.style.color = "white";
-        linkInner2.style.fontSize = "20px";
-        linkInner2.style.fontFamily = "Arial";
-
+        linkInner2.innerHTML = "CONTACT";
+        linkInner2.className = "link";
         link2.appendChild(linkInner2);
 
-    const nameDiv = document.createElement('div');
-    nameDiv.id = "nameDiv";
-    nameDiv.style.height = "110px";
-    nameDiv.style.width = "270px";
-    //nameDiv.style.margin = "auto";
-    nameDiv.style.display = "flex";
-    nameDiv.style.border = "10px solid white";
-
-    const div3 = document.createElement('div');
-    div3.style.margin = "auto";
-    div3.innerHTML = "Dodo & Coco";
-    div3.style.fontSize = "40px";
-    div3.style.color = "white";
-    div3.style.fontWeight = "bold";
-    div3.style.fontFamily = "Arial";
-
-    const link3 = document.createElement('div');
-    link3.id = "link3";
-    link3.style.flexBasis = "10%";
-    link3.style.height = "50px";
-    link3.innerHTML = "LINK 3";
-    link3.style.fontSize = "20px";
-    link3.style.color = "white";
-    link3.style.fontFamily = "Arial";
-
-    const link4 = document.createElement('div');
-    link4.id = "link4";
-    link4.style.flexBasis = "10%";
-    link4.style.height = "50px";
-    link4.innerHTML = "LINK 4";
-    link4.style.fontSize = "20px";
-    link4.style.color = "white";
-    link4.style.fontFamily = "Arial";
-
-    const middleDiv = document.createElement('div');
-    middleDiv.style.display = "flex";
-    middleDiv.style.gridRowStart = "2";
-    middleDiv.style.gridRowEnd = "3";
+    const lowerDiv = document.createElement('div');
+    lowerDiv.style.display = "flex";
+    lowerDiv.style.gridRowStart = "2";
+    lowerDiv.style.gridRowEnd = "3";
 
     
-    const middle = document.createElement('div');
-    middle.style.border = "10px solid white";
-    middle.style.display = "flex";
-    middle.style.width = "1000px";
-    middle.style.height = "500px";
-    middle.style.margin = "auto";
-    middle.style.opacity = ".25";
-    middle.style.backgroundColor = "white";
+    const lowerDisplay = document.createElement('div');
+    //lowerDisplay.style.border = "10px solid rgba(255, 255, 255, 0.5)";
+    lowerDisplay.style.display = "flex";
+    lowerDisplay.style.width = "1000px";
+    lowerDisplay.style.height = "500px";
+    lowerDisplay.style.margin = "auto";
+    lowerDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
 
-    middleDiv.appendChild(middle)
-    nameDiv.appendChild(div3);
+    const lowerItem1 = document.createElement('div');
+    lowerItem1.style.flexBasis = "32%";
+    lowerItem1.style.height = "480px";
+    lowerItem1.style.backgroundImage = "url(../photos/breakfast.jpg)"
+    lowerItem1.style.backgroundSize = "cover";
+    lowerItem1.style.margin = "auto";
+
+    const lowerItem2 = document.createElement('div');
+    lowerItem2.style.flexBasis = "32%";
+    lowerItem2.style.height = "480px";
+    lowerItem2.style.backgroundImage = "url(../photos/pour.jpg)"
+    lowerItem2.style.backgroundSize = "cover";
+    lowerItem2.style.margin = "auto";
+
+    const lowerItem3 = document.createElement('div');
+    lowerItem3.style.flexBasis = "32%";
+    lowerItem3.style.height = "480px";
+    lowerItem3.style.backgroundImage = "url(../photos/cake.jpg)"
+    lowerItem3.style.backgroundSize = "cover";
+    lowerItem3.style.margin = "auto";
+
+    lowerDisplay.appendChild(lowerItem1);
+    lowerDisplay.appendChild(lowerItem2);
+    lowerDisplay.appendChild(lowerItem3);
+    lowerDiv.appendChild(lowerDisplay)
+    nameDiv.appendChild(nameText);
     topDiv.appendChild(link1);
-    topDiv.appendChild(link2);
     topDiv.appendChild(nameDiv);
-    topDiv.appendChild(link3);
-    topDiv.appendChild(link4);
+    topDiv.appendChild(link2);
     topWrapper.appendChild(topDiv);
     containerDiv.appendChild(topWrapper);
-    containerDiv.appendChild(middleDiv);
+    containerDiv.appendChild(lowerDiv);
+
+    var linksClass = topDiv.querySelectorAll('.link');
+    for (let i=0; i<linksClass.length; i++) {
+        linksClass[i].style.margin = "auto";
+        linksClass[i].style.color = "#dbc1ac";
+        linksClass[i].style.fontSize = "25px";
+        linksClass[i].style.fontFamily = "Arial";
+        linksClass[i].style.fontWeight = "bold";
+        linksClass[i].addEventListener("mouseover", addHoverEffects);
+        linksClass[i].addEventListener("mouseout", removeHoverEffects);
+    };
+    var titleLinks = topDiv.querySelectorAll('.title');
+    for (let i=0; i<titleLinks.length; i++) {
+        titleLinks[i].addEventListener("mouseover", addTitleEffects);
+        titleLinks[i].addEventListener("mouseout", removeTitleEffects);
+    }
+    function addTitleEffects() {
+        nameText.style.color = "#ece0d1";
+        this.style.borderColor = "#ece0d1";
+    }
+    function removeTitleEffects() {
+        nameText.style.color = "white";
+        this.style.borderColor = "white";
+    }
+    function addHoverEffects() {
+        //this.style.fontWeight = "bold";
+        this.style.transform = "scale(1.1)";
+        this.style.color = "#ece0d1";
+    }
+    function removeHoverEffects() {
+        //this.style.fontWeight = "normal";
+        this.style.transform = "scale(1)";
+        this.style.color = "#dbc1ac";
+    }
 
     return containerDiv;
 }
