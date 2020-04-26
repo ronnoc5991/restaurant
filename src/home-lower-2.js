eead257b38cc2e1b11c45c924d5947429ba82ce6
Connor <cstreng95@gmail.com>
2020-04-26 08:30:04 +0200

    adds other flip cards to home index as imported modules... all contain lorem ipsum and stylings

---
+++ src/home-lower-2.js
@@ -0,0 +1,55 @@
+const lowerItem2 = () => {
+
+const lowerItem = document.createElement('div');
+    lowerItem.className = "flip-box";
+    lowerItem.style.margin = "auto";
+    lowerItem.style.flexBasis = "32%";
+    lowerItem.style.height = "480px";
+    lowerItem.style.perspective = "1100px";
+    lowerItem.style.backgroundColor = "transparent";
+
+    const flipBoxInner = document.createElement('div');
+    lowerItem.appendChild(flipBoxInner);
+    flipBoxInner.className = "flip-box-inner";
+    flipBoxInner.style.position = "relative";
+    flipBoxInner.style.width = "100%";
+    flipBoxInner.style.height = "100%";
+    flipBoxInner.style.textAlign = "center";
+    flipBoxInner.style.transition = "transform 0.7s";
+    flipBoxInner.style.transformStyle = "preserve-3d";
+
+    const flipBoxFront = document.createElement('div');
+    flipBoxFront.style.position = "absolute";
+    flipBoxFront.style.width = "100%";
+    flipBoxFront.style.height = "100%";
+    flipBoxFront.style.backfaceVisibility = "hidden";
+    flipBoxFront.style.backgroundColor = "black";
+    flipBoxFront.style.backgroundImage = "url(../photos/pour.jpg)";
+    flipBoxFront.style.backgroundSize = "cover";
+    flipBoxInner.appendChild(flipBoxFront);
+
+    const flipBoxBack = document.createElement('div')
+    flipBoxBack.style.display = "flex";
+    flipBoxBack.style.position = "absolute";
+    flipBoxBack.style.width = "100%";
+    flipBoxBack.style.height = "100%";
+    flipBoxBack.style.backfaceVisibility = "hidden";
+    flipBoxBack.style.backgroundColor = "white";
+    flipBoxBack.style.transform = "rotateY(180deg)";
+    flipBoxInner.appendChild(flipBoxBack);
+
+    const backContent = document.createElement('div');
+    backContent.style.margin = "auto";
+    backContent.style.color = "#38220f";
+    backContent.style.fontSize = "20px";
+    backContent.style.paddingLeft = "50";
+    backContent.style.paddingRight = "50";
+    backContent.style.fontFamily = "Arial";
+    backContent.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies integer quis auctor elit sed vulputate mi. Quis eleifend quam adipiscing vitae proin.";
+    flipBoxBack.appendChild(backContent);
+
+
+    return lowerItem
+}
+
+export default lowerItem2
--- src/home.js
+++ src/home.js
@@ -1,4 +1,7 @@
-import mmenu from './menu';
+//import menu from './menu';
+import lowerItem1 from './home-lower-1';
+import lowerItem2 from './home-lower-2';
+import lowerItem3 from './home-lower-3';
 
 const home = () => {
     
@@ -10,49 +13,9 @@ const home = () => {
     lowerDisplay.style.margin = "auto";
     lowerDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
 
-    const lowerItem1 = document.createElement('div');
-    lowerItem1.className = "lowerImage clickable";
-    lowerItem1.style.flexBasis = "32%";
-    lowerItem1.style.height = "480px";
-    lowerItem1.style.backgroundImage = "url(../photos/breakfast.jpg)"
-    lowerItem1.style.backgroundSize = "cover";
-    lowerItem1.style.margin = "auto";
-
-    const lowerItem2 = document.createElement('div');
-    lowerItem2.className = "lowerImage clickable";
-    lowerItem2.style.flexBasis = "32%";
-    lowerItem2.style.height = "480px";
-    lowerItem2.style.backgroundImage = "url(../photos/pour.jpg)"
-    lowerItem2.style.backgroundSize = "cover";
-    lowerItem2.style.margin = "auto";
-
-    const lowerItem3 = document.createElement('div');
-    lowerItem3.className = "lowerImage clickable";
-    lowerItem3.style.flexBasis = "32%";
-    lowerItem3.style.height = "480px";
-    lowerItem3.style.backgroundImage = "url(../photos/cake.jpg)"
-    lowerItem3.style.backgroundSize = "cover";
-    lowerItem3.style.margin = "auto";
-
-    lowerDisplay.appendChild(lowerItem1);
-    lowerDisplay.appendChild(lowerItem2);
-    lowerDisplay.appendChild(lowerItem3);
-
-    const lowerItems = lowerDisplay.querySelectorAll('.lowerImage');
-    for (let i = 0; i<lowerItems.length; i++) {
-        lowerItems[i].addEventListener("mouseover", lowerHoverOn);
-        lowerItems[i].addEventListener("mouseout", lowerHoverOff);
-    }
-
-    function lowerHoverOn() {
-        this.style.transform = "scale(1.01)";
-        this.style.cursor = "pointer";
-    }
-
-    function lowerHoverOff() {
-        this.style.transform = "scale(1)";
-        this.style.cursor = "auto";
-    }
+    lowerDisplay.appendChild(lowerItem1());
+    lowerDisplay.appendChild(lowerItem2());
+    lowerDisplay.appendChild(lowerItem3());
 
     return lowerDisplay;
 }
